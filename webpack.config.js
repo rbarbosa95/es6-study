8679188135e52daa692111e85a802ae580355b34
Ramon Barbosa <designerramon@gmail.com>
2020-08-20 23:02:05 -0300

    Inital commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: ['@babel/polyfill', './src/main.js'],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: __dirname + '/public'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
